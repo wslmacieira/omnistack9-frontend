cb4a6286b8abc2d492f9e777e92df5833eadaeae
wslmacieira <wslmacieira@outlook.com>
2019-10-02 12:10:01 -0300

    adicionado axios e configurado api.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
@@ -12,14 +25,18 @@ function App() {
           Ofereça <strong>spots</strong> para progrmadores e encontre{' '}
           <strong>talentos</strong> para sua empresa
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-Mail *</label>
-          <input 
-            id="email" 
-            type="email" 
+          <input
+            id="email"
+            type="email"
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
-          <button className="btn" type="submit">Entrar</button>
+          <button className="btn" type="submit">
+            Entrar
+          </button>
         </form>
       </div>
     </div>
